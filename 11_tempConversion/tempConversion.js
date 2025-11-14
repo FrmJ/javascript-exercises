ae9fab88efb11504967287c581b5a35b32eb5629
FrmJ <70322094+FrmJ@users.noreply.github.com>
2025-11-13 18:20:18 -0800

    do 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const convertedTemp = (temp - 32) * 5 / 9; 
+  return hasDecimal(convertedTemp) ? parseFloat(convertedTemp.toFixed(1))
+        : convertedTemp; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const convertedTemp = (temp * 9 / 5) + 32;
+  return hasDecimal(convertedTemp) ? parseFloat(convertedTemp.toFixed(1))
+        : convertedTemp;
 };
+
+const hasDecimal = function(temp) {
+  return temp % 1 !== 0;
+}
 
 // Do not edit below this line
 module.exports = {
