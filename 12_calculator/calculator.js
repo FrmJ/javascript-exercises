c745b79bb9af3993718ce45fc1de0cf8a4e64ce0
FrmJ <70322094+FrmJ@users.noreply.github.com>
2026-06-23 16:45:05 -0700

    do 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, current) => current + acc, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc * current, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0 || x == 1) {
+    return 1;
+  }
+
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
