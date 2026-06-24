c5fd68bb7b27ecc91d213d9d19550f33dfbde7d5
FrmJ <70322094+FrmJ@users.noreply.github.com>
2026-06-23 17:35:29 -0700

    do 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "")
+                        .toLowerCase();
+    const reversedStr = cleanedStr
+                        .split("")
+                        .reverse()
+                        .join("");
+    console.log(cleanedStr);
+    console.log(reversedStr);
+    return cleanedStr == reversedStr;
 };
-
+console.log(palindromes("racecar!"));
 // Do not edit below this line
 module.exports = palindromes;
