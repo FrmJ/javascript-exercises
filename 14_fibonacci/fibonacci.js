2eef0c38dbed67e57b2dafc3646c555690c1dcb7
FrmJ <70322094+FrmJ@users.noreply.github.com>
2026-06-28 15:53:34 -0700

    do 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) {
+        return "OOPS";
+    }
 
+    let num = parseInt(x);
+
+    if (num <= 1) {
+        return num;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
