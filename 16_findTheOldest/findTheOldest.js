7676a67f61c402343777859b28f6429601ee4f3e
FrmJ <70322094+FrmJ@users.noreply.github.com>
2026-06-28 16:36:08 -0700

    do 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    return people.map(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = currentYear;
+        }
+        return person;
+    }).sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1)[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
